e3ffd9ed2e0a0e4365acb0611ce3dcecc1b0b0b1
Ale Paz <evelia.salgadopaz@gmail.com>
2023-05-02 15:53:38 +0200

    Some min-max values in the form to avoid negative numbers

--- client/src/components/addOrder/AddOrder.tsx
+++ client/src/components/addOrder/AddOrder.tsx
@@ -22,7 +22,7 @@ export default function AddOrder() {
     date: '',
     payment: '',
     fullfilment: '',
-    transit: ''
+    delivery: ''
   })
 
   const handleChange = (e) => {
@@ -40,6 +40,7 @@ export default function AddOrder() {
     // const baseUrl = import.meta.env.VITE_BASE_URL
 
     const postOrder = async (order) => {
+      console.log(order)
       const response = await fetch(baseUrl + '/orders', {
         method: "POST",
         body: JSON.stringify(order),
@@ -68,22 +69,22 @@ export default function AddOrder() {
         <h1>New Order</h1>
 
         <label htmlFor='id'>ID</label>
-        <input type='number' id='id' name='id' value={state.id} onChange={handleChange}></input>
+        <input type='number' id='id' name='id' min='0' max='1000000' value={state.id} onChange={handleChange} required></input>
 
         <label htmlFor='ourClient'>Our client</label>
-        <input type='text' id='ourClient' name='ourClient' value={state.ourClient} onChange={handleChange}></input>
+        <input type='text' id='ourClient' name='ourClient' value={state.ourClient} onChange={handleChange} required></input>
 
         <label htmlFor='date'>Date</label>
-        <input type='datetime-local' id='date' name='date' value={state.date} onChange={handleChange}></input>
+        <input type='datetime-local' id='date' name='date' min={new Date().toISOString().slice(0,16)} value={state.date} onChange={handleChange} required></input>
 
         <label htmlFor='quantity'>Quantity</label>
-        <input type='number' id='quantity' name='quantity' value={state.quantity} onChange={handleChange}></input>
+        <input type='number' id='quantity' name='quantity' min='0' max='1000000' value={state.quantity} onChange={handleChange} required></input>
 
         <label htmlFor='charge'>Charge</label>
-        <input type='number' id='charge' name='charge' value={state.charge} onChange={handleChange}></input>
+        <input type='number' id='charge' name='charge' min='0' max='10000000' value={state.charge} onChange={handleChange} required></input>
 
         <label htmlFor='finalClient'>Final client</label>
-        <input type='text' id='finalClient' name='finalClient' value={state.finalClient} onChange={handleChange}></input>
+        <input type='text' id='finalClient' name='finalClient' value={state.finalClient} onChange={handleChange} required></input>
 
         <div className={styles.selectors}>
 
@@ -106,7 +107,7 @@ export default function AddOrder() {
           </div>
 
           <div className={styles.selector}>
-            <label htmlFor='delivery'>Transit</label>
+            <label htmlFor='delivery'>Delivery</label>
             <select className={styles.select} name='delivery' value={state.delivery} onChange={handleChange}>
               {delivery_status.map(status => (
                 <option key={status.id} value={status.status}>{status.status}</option>
