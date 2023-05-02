a0ef54d61905e345b9cf8766cb1f97d755730e53
Aaron <sherrilla71940@gmail.com>
2023-05-02 18:41:55 +0800

    add prettier

--- server/controller/controller.js
+++ server/controller/controller.js
@@ -1,112 +1,116 @@
-const { getAll, postOne, updateOne, createUser, logInUser } = require('../model/model')
-const jwt = require('jsonwebtoken')
-
+const {
+  getAll,
+  postOne,
+  updateOne,
+  createUser,
+  logInUser,
+} = require("../model/model");
+const jwt = require("jsonwebtoken");
 
 const createToken = (id) => {
-  return jwt.sign({ id }, 'secret', {
+  return jwt.sign({ id }, "secret", {
     expiresIn: "1d",
-    algorithm: "HS256"
-  })
-}
+    algorithm: "HS256",
+  });
+};
 
 const getOrders = async (req, res) => {
   try {
-    console.log('reached get orders controller')
-    const orders = await getAll()
-    res.status(201)
-    res.send(orders)
+    console.log("reached get orders controller");
+    const orders = await getAll();
+    res.status(201);
+    res.send(orders);
   } catch (error) {
-    console.log(error)
-    res.status(400)
-    res.send('stuck in controller')
+    console.log(error);
+    res.status(400);
+    res.send("stuck in controller");
   }
-}
+};
 
 const postOrder = (req, res) => {
-  console.log('reached the controller', req.body)
+  console.log("reached the controller", req.body);
   try {
-    const order = postOne(req.body)
-    res.status(201)
-    res.send(order)
+    const order = postOne(req.body);
+    res.status(201);
+    res.send(order);
   } catch (error) {
-    console.log(error)
-    res.status(400)
-    res.send('stuck in controller')
+    console.log(error);
+    res.status(400);
+    res.send("stuck in controller");
   }
-}
+};
 
 const putOrder = async (req, res) => {
   try {
-    const order = await updateOne(req.body)
+    const order = await updateOne(req.body);
     // res.json({message: 'ok'})
-    res.send(order)
+    res.send(order);
   } catch (error) {
-    console.log(error)
+    console.log(error);
   }
-}
+};
 
 const signUp = async (req, res) => {
   const { email, password } = req.body;
   let responseUser;
   try {
-    responseUser = await createUser(email, password)
+    responseUser = await createUser(email, password);
     // console.log('responseuser', responseUser)
-    console.log('successful signup')
+    console.log("successful signup");
 
     if (responseUser._id) {
-      const token = createToken(responseUser._id)
-      res.status(201)
-      res.cookie('jwt', token, {
+      const token = createToken(responseUser._id);
+      res.status(201);
+      res.cookie("jwt", token, {
         httpOnly: true,
-        sameSite: 'strict',
+        sameSite: "strict",
         maxAge: 86400, // 1 day
         secure: false,
-        domain: 'localhost',
-        path: '/'
+        domain: "localhost",
+        path: "/",
         // maxAge: maxAge * 1000
-      })
-      console.log('cookie has supposedly been set');
-      res.json({ user: responseUser._id })
+      });
+      console.log("cookie has supposedly been set");
+      res.json({ user: responseUser._id });
     } else {
-      res.json({ responseUser })
+      res.json({ responseUser });
     }
   } catch (error) {
-    res.status(400)
-    res.json(error) // ?
+    res.status(400);
+    res.json(error); // ?
   }
-}
-
+};
 
 const logIn = async (req, res) => {
-  const { email, password } = req.body
-  console.log('login reached', req.body)
+  const { email, password } = req.body;
+  console.log("login reached", req.body);
   try {
-    const response = await logInUser(email, password)
+    const response = await logInUser(email, password);
 
     if (response._id) {
-      console.log('entered')
+      console.log("entered");
 
-      res.status(200)
-      const token = createToken(response._id)
+      res.status(200);
+      const token = createToken(response._id);
       console.log(token);
-      res.cookie('jwt', token, {
+      res.cookie("jwt", token, {
         httpOnly: true,
-        sameSite: 'strict',
+        sameSite: "strict",
         secure: false,
         maxAge: 86400, // 1 day
-        domain: 'localhost',
-        path: '/'
+        domain: "localhost",
+        path: "/",
         // maxAge: maxAge * 1000
-      })
-      res.json({ user: response._id })
+      });
+      res.json({ user: response._id });
     } else {
-      res.json({ response })
+      res.json({ response });
     }
   } catch (error) {
-    console.log(error)
-    res.status(400)
-    res.json(error) // ?
+    console.log(error);
+    res.status(400);
+    res.json(error); // ?
   }
-}
+};
 
-module.exports = { getOrders, postOrder, putOrder, signUp, logIn }
+module.exports = { getOrders, postOrder, putOrder, signUp, logIn };
