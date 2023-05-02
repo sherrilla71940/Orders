d989fdefc22add0d36e6dd09496a9b2f0cec46b8
Xavier Malbran <malbranjavier@gmail.com>
2023-05-02 10:56:33 +0200

    changed main.jsx to tsx

--- client/src/main.tsx
+++ client/src/main.tsx
@@ -7,7 +7,7 @@ import { Provider } from "react-redux";
 
 import './index.css'
 
-ReactDOM.createRoot(document.getElementById('root')).render(
+ReactDOM.createRoot(document.getElementById('root')!).render(
   <Provider store={store}>
     <React.StrictMode>
       <App />
