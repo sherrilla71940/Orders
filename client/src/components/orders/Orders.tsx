e1b410024f5560df916dae013eb5b4c77ddad914
Aaron <sherrilla71940@gmail.com>
2023-05-04 01:01:59 +0800

    commit

--- client/src/components/orders/Orders.tsx
+++ client/src/components/orders/Orders.tsx
@@ -1,7 +1,7 @@
-import styles from './Orders.module.css'
+import styles from "./Orders.module.css";
 import { useSelector, useDispatch } from "react-redux";
 import { updateOrder } from "../../store/actions";
-import Order from '../../Order'
+import Order from "../../Order";
 
 import {
   dateFormater,
@@ -11,78 +11,75 @@ import {
   stage_1,
   stage_2,
   stage_3,
-  formatCurrency
-} from '../../utilities/utilities'
-import { Value } from '@radix-ui/react-select';
+  formatCurrency,
+} from "../../utilities/utilities";
+import { Value } from "@radix-ui/react-select";
 
 type idAndProcess = {
-  id: string,
-  payment?: string,
-  fullfilment?: string,
-  delivery?: string
-}
+  id: string;
+  payment?: string;
+  fullfilment?: string;
+  delivery?: string;
+};
 
 export default function Orders() {
-
-  const dispatch = useDispatch()
+  const dispatch = useDispatch();
 
   // use to change the color in the select HTML tag. Partially implemented, labeled colors are not persisting
-  const handleClass = (e: React.ChangeEvent<HTMLSelectElement> ) => {
-  // const handleClass = (e) => {
-    const value = e.target.value
-    console.log(Value)
+  const handleClass = (e: React.ChangeEvent<HTMLSelectElement>) => {
+    // const handleClass = (e) => {
+    const value = e.target.value;
+    console.log(Value);
     if (stage_1.includes(value)) {
-      e.target.classList.add(`${styles.bg_stage_1}`)
-      e.target.classList.remove(`${styles.bg_stage_2}`)
-      e.target.classList.remove(`${styles.bg_stage_3}`)
+      e.target.classList.add(`${styles.bg_stage_1}`);
+      e.target.classList.remove(`${styles.bg_stage_2}`);
+      e.target.classList.remove(`${styles.bg_stage_3}`);
     }
     if (stage_2.includes(value)) {
-      e.target.classList.add(`${styles.bg_stage_2}`)
-      e.target.classList.remove(`${styles.bg_stage_1}`)
-      e.target.classList.remove(`${styles.bg_stage_3}`)
+      e.target.classList.add(`${styles.bg_stage_2}`);
+      e.target.classList.remove(`${styles.bg_stage_1}`);
+      e.target.classList.remove(`${styles.bg_stage_3}`);
     }
     if (stage_3.includes(value)) {
-      e.target.classList.add(`${styles.bg_stage_3}`)
-      e.target.classList.remove(`${styles.bg_stage_1}`)
-      e.target.classList.remove(`${styles.bg_stage_2}`)
+      e.target.classList.add(`${styles.bg_stage_3}`);
+      e.target.classList.remove(`${styles.bg_stage_1}`);
+      e.target.classList.remove(`${styles.bg_stage_2}`);
     }
-   
-  }
+  };
 
   // const orders: Order[] = useSelector((state) => state.orders);
   const orders = useSelector((state: any) => state.orders);
- 
-  const baseUrl = 'http://localhost:3000'
+
+  const baseUrl = "http://localhost:3000";
   const putOrder = async (idAndProcess: idAndProcess) => {
-      
-      const response = await fetch(baseUrl + '/orders', {
-        method: "PUT",
-        body: JSON.stringify(idAndProcess),
-        headers: {
-          "Content-type": "application/json"
-        }
-      })
-        .then(response => {
+    const response = await fetch(baseUrl + "/orders", {
+      method: "PUT",
+      body: JSON.stringify(idAndProcess),
+      headers: {
+        "Content-type": "application/json",
+      },
+    })
+      .then((response) => {
         if (response.ok) {
-          return response.json()
+          return response.json();
         }
-        throw new Error('unable to fetch data')
+        throw new Error("unable to fetch data");
       })
-      
-      .then(data => {
-        dispatch(updateOrder(data))
+
+      .then((data) => {
+        dispatch(updateOrder(data));
         // console.log(data)
       })
-      .catch(error => {
-        console.log('Catched error: ', error)
-      })
-    }
+      .catch((error) => {
+        console.log("Catched error: ", error);
+      });
+  };
 
   const handleChange = async (idAndProcess: idAndProcess) => {
     // console.log(idAndProcess)
-    await putOrder(idAndProcess)
-  }
- 
+    await putOrder(idAndProcess);
+  };
+
   return (
     <>
       <div className={styles.tableWrapper}>
@@ -101,82 +98,84 @@ export default function Orders() {
             </tr>
           </thead>
           <tbody className={styles.tableBody}>
-
             {
               // orders.map((order) => (
               orders.map((order: Order) => (
                 <tr key={order.id} className={styles.tableRow}>
                   <td>{order.id}</td>
                   <td>{order.ourClient}</td>
-                  <td>{dateFormater(order.date)}</td>
+                  <td>{dateFormater(order.date as Date)}</td>
                   <td>{order.quantity}</td>
                   <td>{formatCurrency(order.charge)}</td>
 
-                  <td className={styles.selectContainer}>{
-                    <select
-                      className={styles.select}
-                      name='payment' value={order.payment}
-                      onChange={
-                        (e) => {
+                  <td className={styles.selectContainer}>
+                    {
+                      <select
+                        className={styles.select}
+                        name="payment"
+                        value={order.payment}
+                        onChange={(e) => {
                           handleChange({
-                            id: order.id, payment: e.target.value
+                            id: order.id,
+                            payment: e.target.value,
                           });
-                          handleClass(e)
-                        }
-                      }
-                    >
-                      {payment_status.map(status => (
-                        <option key={status.id} value={status.status}>{status.status}</option>
-                      ))}
-                    </select>
-                  }</td>
+                          handleClass(e);
+                        }}
+                      >
+                        {payment_status.map((status) => (
+                          <option key={status.id} value={status.status}>
+                            {status.status}
+                          </option>
+                        ))}
+                      </select>
+                    }
+                  </td>
 
-                  <td className={styles.selectContainer}>{
-                    <select
-                      className={styles.select}
-                      name='fullfilment' value={order.fullfilment}
-                      onChange={
-                        (e) => {
+                  <td className={styles.selectContainer}>
+                    {
+                      <select
+                        className={styles.select}
+                        name="fullfilment"
+                        value={order.fullfilment}
+                        onChange={(e) => {
                           handleChange({
-                            id: order.id, fullfilment: e.target.value
+                            id: order.id,
+                            fullfilment: e.target.value,
                           });
-                          handleClass(e)
-                        }
-                      }
-                    >
-                      {fullfilment_status.map(status => (
-                        <option key={status.id} value={status.status}>{status.status}</option>
-                      ))}
-                    </select>
-                  }</td>
+                          handleClass(e);
+                        }}
+                      >
+                        {fullfilment_status.map((status) => (
+                          <option key={status.id} value={status.status}>
+                            {status.status}
+                          </option>
+                        ))}
+                      </select>
+                    }
+                  </td>
 
                   <td>{order.finalClient}</td>
 
-                  <td className={styles.selectContainer} >
-                  <select
-                    className={styles.select}
-                    name='delivery'
-                    value={order.delivery}
-                    onChange={
-                      (e) => {
+                  <td className={styles.selectContainer}>
+                    <select
+                      className={styles.select}
+                      name="delivery"
+                      value={order.delivery}
+                      onChange={(e) => {
                         handleChange({
-                          id: order.id, delivery: e.target.value
+                          id: order.id,
+                          delivery: e.target.value,
                         });
-                        handleClass(e)
-                      }
-                    }
-                  >
-                    {delivery_status.map(status => (
-                      <option
-                        key={status.id}
-                        value={status.status}
-                      >
-                        {status.status}
-                      </option>
-                    ))}
-                  </select>
-                </td>
-
+                        handleClass(e);
+                      }}
+                    >
+                      {delivery_status.map((status) => (
+                        <option key={status.id} value={status.status}>
+                          {status.status}
+                        </option>
+                      ))}
+                    </select>
+                  </td>
                 </tr>
               ))
             }
@@ -184,5 +183,5 @@ export default function Orders() {
         </table>
       </div>
     </>
-  )
+  );
 }
